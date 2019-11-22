93943ccf833276a9249a9a8c809d43b2ee263b7f
Kenny Tanglao <tanglaok@jbb.com>
2019-11-22 10:52:43 -0500

    conf.py

--- conf.py
+++ conf.py
@@ -154,7 +154,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'THRUX.tex', 'TTHRUX Implementation Guide',
+    (master_doc, 'THRUX.tex', 'THRUX Implementation Guide',
      'KT', 'manual'),
 ]
 
